51e2467816a6e7db73e981b352e16ff143099932
Mauro <kubuni78@gmail.com>
2018-01-04 15:20:49 +0100

    update reverse string file with some explanations

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -6,18 +6,28 @@ const darthVader = "Luke, I'm your father!";
 const yoda = "Do or Do Not, There is No Try";
 
 function reverseString(str) {
-    //we need to split in an array our string to reverse it later.
+    //we need to split our string in an array to reverse it later.
     //only on arrays we can use reverse method...so let's SPLIT the string!
+    
+    //Pay Attention: different ways to split:
+    //const str = "Hello World"
+    //str.split() will give you back this: 
+    //['Hello World'];
+    //split('hello world') will give you back this (what we need here): 
+    //[ 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' ];
+    //str.split(" ") will give you back this:
+    //[ 'Hello', 'World' ];
+
     return str.split('')
               .reverse() //now we reverse it! So Easy and powerful!
               .join(''); //finally let's join the reversed array, to have our reversed string;
+//we can write it down also in one line:
+//return str.split('').reverse().join('');
 }
 
 console.log(reverseString(coleridge));
 console.log(reverseString(dante));
 console.log(reverseString(darthVader));
 console.log(reverseString(yoda);
-
-
 
   
